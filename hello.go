0ff80347bc96c760d0e57d6bcc2b8e7718cf7783
vicfirth@1997 <yashmundra1997@gmail.com>
2024-03-23 12:44:02 +0530

    about variables

--- hello.go
+++ hello.go
@@ -2,6 +2,25 @@ package main
 
 import "fmt"
 
+// if we want to declare a variable at the packagae level then we cant use the := syntax.
+
+var a int=24
+
 func main() {
     fmt.Println("Hello, World!");
+    var i int // just declaring the variable
+    i=42
+    fmt.Println(i)
+    //or 
+    var j int =27
+    fmt.Println(j)
+    //or
+    k:=99
+    fmt.Println(k)
+    // printing the value and type of the variable
+    fmt.Printf("%v, %T",j,j)
+
+    //a will be accessible which is defined outside the main().
+    fmt.Println(a)
+
 }
